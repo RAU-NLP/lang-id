ec87e691095d6cb3b181a2352e3bbbc013c9c8f1
Adam Mathias Bittlingmayer <bittlingmayer@users.noreply.github.com>
2018-03-27 22:29:00 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ for dir in data/*/
 do
 
   printf "\n\nTRAINING MODEL $dir...\n"
-  ./fastText/fasttext supervised -input $dir/train.ft.txt -output $dir/model -verbose 3 -dim 400
+  ./fastText/fasttext supervised -input $dir/train.ft.txt -output $dir/model -verbose 3
 
   printf "\n\nTESTING MODEL $dir...\n"
   ./fastText/fasttext test $dir/model.bin $dir/test.ft.txt
